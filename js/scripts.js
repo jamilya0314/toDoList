5745a281c721ac2636e3ece4629f7b97b2c1be31
Jamilya0314e <jamilya0393@gmail.com>
2023-07-09 22:08:52 +0200

    add js/scripts.js,index.html,db.json

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector("ul")
+const addInput = document.querySelector(".add_input")
+const addButton = document.querySelector("add_button")
+
+const state = {
+    todos: []
+};
